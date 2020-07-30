54041cfeb3253f002913d00ec4fd48e522f15e10
Abdullah Ilgaz <mail@abdullahilgaz.com>
2020-07-30 21:46:35 +0300

    Initial commit

---
+++ zb-noty/Noty.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace zb_noty
+{
+    partial class Noty
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BorderLeft = new System.Windows.Forms.Panel();
+            this.Caption = new System.Windows.Forms.Label();
+            this.BorderTop = new System.Windows.Forms.Panel();
+            this.BorderBottom = new System.Windows.Forms.Panel();
+            this.BorderRight = new System.Windows.Forms.Panel();
+            this.Message = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BorderLeft
+            // 
+            this.BorderLeft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(188)))), ((int)(((byte)(119)))));
+            this.BorderLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.BorderLeft.Location = new System.Drawing.Point(0, 0);
+            this.BorderLeft.Margin = new System.Windows.Forms.Padding(4);
+            this.BorderLeft.Name = "BorderLeft";
+            this.BorderLeft.Size = new System.Drawing.Size(50, 86);
+            this.BorderLeft.TabIndex = 0;
+            // 
+            // Caption
+            // 
+            this.Caption.AutoSize = true;
+            this.Caption.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Caption.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(188)))), ((int)(((byte)(119)))));
+            this.Caption.Location = new System.Drawing.Point(52, 19);
+            this.Caption.Name = "Caption";
+            this.Caption.Size = new System.Drawing.Size(126, 25);
+            this.Caption.TabIndex = 1;
+            this.Caption.Text = "İşlem başarılı";
+            // 
+            // BorderTop
+            // 
+            this.BorderTop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(188)))), ((int)(((byte)(119)))));
+            this.BorderTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BorderTop.Location = new System.Drawing.Point(50, 0);
+            this.BorderTop.Margin = new System.Windows.Forms.Padding(4);
+            this.BorderTop.Name = "BorderTop";
+            this.BorderTop.Size = new System.Drawing.Size(457, 1);
+            this.BorderTop.TabIndex = 2;
+            // 
+            // BorderBottom
+            // 
+            this.BorderBottom.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(188)))), ((int)(((byte)(119)))));
+            this.BorderBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.BorderBottom.ForeColor = System.Drawing.Color.White;
+            this.BorderBottom.Location = new System.Drawing.Point(50, 85);
+            this.BorderBottom.Margin = new System.Windows.Forms.Padding(4);
+            this.BorderBottom.Name = "BorderBottom";
+            this.BorderBottom.Size = new System.Drawing.Size(457, 1);
+            this.BorderBottom.TabIndex = 3;
+            // 
+            // BorderRight
+            // 
+            this.BorderRight.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(188)))), ((int)(((byte)(119)))));
+            this.BorderRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.BorderRight.Location = new System.Drawing.Point(506, 1);
+            this.BorderRight.Margin = new System.Windows.Forms.Padding(4);
+            this.BorderRight.Name = "BorderRight";
+            this.BorderRight.Size = new System.Drawing.Size(1, 84);
+            this.BorderRight.TabIndex = 4;
+            // 
+            // Message
+            // 
+            this.Message.AutoSize = true;
+            this.Message.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Message.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(188)))), ((int)(((byte)(119)))));
+            this.Message.Location = new System.Drawing.Point(53, 47);
+            this.Message.Name = "Message";
+            this.Message.Size = new System.Drawing.Size(308, 21);
+            this.Message.TabIndex = 5;
+            this.Message.Text = "Kayıt işlemi başarılı bir şekilde tamamlandı.";
+            // 
+            // NotyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(249)))), ((int)(((byte)(239)))));
+            this.ClientSize = new System.Drawing.Size(507, 86);
+            this.ControlBox = false;
+            this.Controls.Add(this.Message);
+            this.Controls.Add(this.BorderRight);
+            this.Controls.Add(this.BorderBottom);
+            this.Controls.Add(this.BorderTop);
+            this.Controls.Add(this.Caption);
+            this.Controls.Add(this.BorderLeft);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "NotyForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel BorderLeft;
+        private System.Windows.Forms.Label Caption;
+        private System.Windows.Forms.Panel BorderTop;
+        private System.Windows.Forms.Panel BorderBottom;
+        private System.Windows.Forms.Panel BorderRight;
+        private System.Windows.Forms.Label Message;
+    }
+}
+
